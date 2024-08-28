b4c3990d87f5e7fffb261c21b563e4354ea454fc
sekarsha <sekarsha09@gmail.com>
2024-08-28 15:14:47 +0530

    uu

--- Back_End/Routes/product.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const { getproduct, getsingle_product } = require("../Controll/product_controll");
-const router=express.Router();
-
-router.route("/product").get(getproduct);
-
-router.route("/product/:id").get(getsingle_product);
-
-module.exports=router;
